10ab17ad2128b0c3bbd7999b8420080e4fa5e301
Natalie Jorion <talie.jorion@gmail.com>
2020-04-15 22:09:18 -0500

    Update app.R
    (1)	Removed the “Number of SE” dropdown 
    (2)	Included three different colored blue error bars 
    (note: I purposely pushed this element to the back; when the rectangles are at the front, the rest of the graph looks muddy)
    (3)	Fixed the issues with the decimals/rounding
    (4)	Cleaned up the row text
    (5)    Added option to specify custom cut score to see the pass rate

--- app.R
+++ app.R
@@ -15,15 +15,19 @@
 
 ##### Install required libraries ####
 
-packs <- c("shiny", "datasets", "ggplot2")
+packs <- c("shiny", "datasets", "ggplot2", "gginnards")
 
-lapply(packs,function(x) {
-  if(x %in% rownames(installed.packages())==FALSE)
-  {install.packages(x)
-    sapply(x,library,character.only=TRUE)}
-  else{sapply(x,library,character.only=TRUE)}
-})
+# lapply(packs,function(x) {
+#   if(x %in% rownames(installed.packages())==FALSE)
+#   {install.packages(x)
+#     sapply(x,library,character.only=TRUE)}
+#   else{sapply(x,library,character.only=TRUE)}
+# })
 
+library(shiny)
+library(datasets)
+library(ggplot2)
+library(gginnards)
 
 #Function for ggplot histogram to specify integer bins
 int_breaks <- function(x, n = 5) pretty(x, n)[pretty(x, n) %% 1 == 0] 
@@ -52,26 +56,31 @@ ui <- shinyUI(fluidPage(
                      sidebarPanel(
                               numericInput("max_score", "Max Score:", 100, min = 1, max = 300, width = '150px'),
                               numericInput("cut_score", "Cut Score:", 0, min = 1, max = 100, width = '150px'),
-                              numericInput("standard_error", "+1 Standard Error:", NA, min = 0, max = 25, width = '150px'),
-                              selectInput("SE_num", "Number of Standard Errors:",
-                                          c("", "1 Standard Error" = 1,
-                                            "2 Standard Errors" = 2,
-                                            "3 Standard Errors" = 3), width = '150px'), width = 2
+                              numericInput("standard_error", "Standard Error:", NA, min = 0, max = 25, width = '150px'),                        
+                          numericInput("optional_cut", "Potential Cut Score:", NA, min = 0, max = NA, width = '150px'),
+                              # selectInput("SE_num", "Number of Standard Errors:",
+                              #             c("", "1 Standard Error" = 1,
+                              #               "2 Standard Errors" = 2,
+                              #               "3 Standard Errors" = 3), width = '150px'), 
+                              width = 2
                          
                      ),
                      mainPanel(
-                         htmlOutput ("pass_fail"),
-                         tags$br(),
-                         htmlOutput ("SE"),
-                         tags$br(),
+                         # htmlOutput ("pass_fail"),
+                         # tags$br(),
+                         # htmlOutput ("SE"),
+                         # tags$br(),
                          plotOutput('MyPlot'),
-                         tags$br(),
-                         tableOutput('SE_output')
+                         br(),
+                         (HTML("<center>")),
+                         h3(tableOutput('SE_output')),
+                         br(),
+                         h3(htmlOutput ("user_cut")),
+                         (HTML("</center>"))
                          
                      )
                  )
         )
-        
     )
 )
 )
@@ -95,8 +104,17 @@ server <- shinyServer(function(input, output, session) {
       
           Scores <- data()[2]
           
-          SE_rect <- data.frame(xmin=(input$cut_score-0.5)-as.numeric(input$SE_num)*input$standard_error, 
-                                xmax=(input$cut_score-0.5)+as.numeric(input$SE_num)*input$standard_error, ymin=-Inf, ymax=Inf)
+          SE_rect1 <- data.frame(xmin=(input$cut_score-0.5)-input$standard_error,
+                                xmax=(input$cut_score-0.5)+input$standard_error, 
+                                ymin=-Inf, ymax=Inf)
+          
+          SE_rect2 <- data.frame(xmin=(input$cut_score-0.5)-(2*input$standard_error),
+                                 xmax=(input$cut_score-0.5)+(2*input$standard_error), 
+                                 ymin=-Inf, ymax=Inf)
+          
+          SE_rect3 <- data.frame(xmin=(input$cut_score-0.5)-(3*input$standard_error),
+                                 xmax=(input$cut_score-0.5)+(3*input$standard_error), 
+                                 ymin=-Inf, ymax=Inf)
           
           status <- factor(ifelse(Scores[1]>(input$cut_score-0.01), "Pass", "Fail"))
       
@@ -108,87 +126,113 @@ server <- shinyServer(function(input, output, session) {
                  scale_fill_manual(values = c("Pass" = "#32CD32", "Fail" = "red")) +
                  scale_x_continuous(limits = c(0,input$max_score), expand = c(0, 0)) +
                  scale_y_continuous(expand = expand_scale(mult=c(0, 0.5)), breaks = int_breaks) +
-                 geom_vline(xintercept = input$cut_score-0.5, linetype = "dashed", color = "red", size = 1) +
-                 geom_rect(data=SE_rect, aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), fill="red", alpha=0.1, inherit.aes = FALSE) +
+                 geom_vline(xintercept = input$cut_score-0.5, linetype = "dashed", 
+                            color = "red", size = 1) +
+                 geom_rect(data=SE_rect1, aes(xmin=xmin, xmax=xmax, ymin=ymin, 
+                                             ymax=ymax), fill="#2a4d69", alpha=0.3, inherit.aes = FALSE) +
+                 geom_rect(data=SE_rect2, aes(xmin=xmin, xmax=xmax, ymin=ymin, 
+                                               ymax=ymax), fill="#4b86b4", alpha=0.3, inherit.aes = FALSE) +
+                geom_rect(data=SE_rect3, aes(xmin=xmin, xmax=xmax, ymin=ymin, 
+                                                ymax=ymax), fill="#adcbe3", alpha=0.3, inherit.aes = FALSE) +
                  theme_bw(base_size = 25) 
                  
-            print(p)
+            print(move_layers(p, "GeomRect", position = "bottom"))
 
     })
     
-    output$pass_fail <- renderText({
-        paste0("<h4>",
-               "When the cut score is set to ", 
-               "<strong>", input$cut_score, "</strong>",", ", 
-               (sum(data()[2]>(input$cut_score-0.01))/nrow(data()))*100,
-               "% of candidates will pass the exam.",
-               "</h4>")
+    output$user_cut <- renderText({
+      if(!is.na(input$cut_score) & !is.na(input$standard_error) & !is.na(input$optional_cut)){
+        paste0("When the cut score is set to ",
+               "<strong>", input$optional_cut, "</strong>",", ",
+               round(((sum(data()[2]>(input$optional_cut-0.01))/nrow(data()))*100),2),
+                   "% of candidates will pass the exam.")
+        }
+        else {
+          paste("")
+        }
     })
     
-    output$SE <- renderText({
-      
-      if(!is.na(input$standard_error)){
-        if(!is.na(input$SE_num)){
-          if(input$SE_num==1){
-             paste0("<h4>",
-             "When the cut score is increased by one standard error (", 
-             "<strong>", input$cut_score+input$standard_error, "</strong>","), ", 
-             (sum(data()[2]>((input$cut_score-0.01)+input$standard_error))/nrow(data()))*100,
-             "% of candidates will pass the exam.","<br><br>",
-             "When the cut score is decreased by one standard error (", 
-             "<strong>", input$cut_score-input$standard_error, "</strong>","), ", 
-             (sum(data()[2]>((input$cut_score-0.01)-input$standard_error))/nrow(data()))*100,
-             "% of candidates will pass the exam.","<br>",
-             "</h4>")
-             }
-          else if(input$SE_num==2){
-            paste0("<h4>",
-            "When the cut score is increased by two standard errors (", 
-             "<strong>", input$cut_score+(input$standard_error*2), "</strong>","), ", 
-             (sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*2)))/nrow(data()))*100,
-             "% of candidates will pass the exam.","<br><br>",
-             "When the cut score is decreased by two standard errors (", 
-             "<strong>", input$cut_score-(input$standard_error*2), "</strong>","), ", 
-             (sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*2)))/nrow(data()))*100,
-             "% of candidates will pass the exam.","<br>",
-             "</h4>")
-          }
-          else if(input$SE_num==3){
-            paste0("<h4>",
-                   "When the cut score is increased by three standard errors (", 
-                   "<strong>", input$cut_score+input$standard_error, "</strong>","), ", 
-                   (sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*3)))/nrow(data()))*100,
-                   "% of candidates will pass the exam.","<br><br>",
-                   "When the cut score is decreased by three standard errors (", 
-                   "<strong>", input$cut_score-input$standard_error, "</strong>","), ", 
-                   (sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*3)))/nrow(data()))*100,
-                   "% of candidates will pass the exam.","<br>",
-                   "</h4>")
-          }
-        }
-      }
-      else {
-        paste("")
-      }
-    })
+    # output$pass_fail <- renderText({
+    #     paste0("<h4>",
+    #            "When the cut score is set to ", 
+    #            "<strong>", input$cut_score, "</strong>",", ", 
+    #            (sum(data()[2]>(input$cut_score-0.01))/nrow(data()))*100,
+    #            "% of candidates will pass the exam.",
+    #            "</h4>")
+    # })
+    
+    # output$SE <- renderText({
+    #   
+      # if(!is.na(input$standard_error)){
+      #   if(!is.na(input$SE_num)){
+      #     if(input$SE_num==1){
+      #        paste0("<h4>",
+      #        "When the cut score is increased by one standard error (", 
+      #        "<strong>", input$cut_score+input$standard_error, "</strong>","), ", 
+      #        (sum(data()[2]>((input$cut_score-0.01)+input$standard_error))/nrow(data()))*100,
+      #        "% of candidates will pass the exam.","<br><br>",
+      #        "When the cut score is decreased by one standard error (", 
+      #        "<strong>", input$cut_score-input$standard_error, "</strong>","), ", 
+      #        (sum(data()[2]>((input$cut_score-0.01)-input$standard_error))/nrow(data()))*100,
+      #        "% of candidates will pass the exam.","<br>",
+      #        "</h4>")
+      #        }
+      #     else if(input$SE_num==2){
+      #       paste0("<h4>",
+      #       "When the cut score is increased by two standard errors (", 
+      #        "<strong>", input$cut_score+(input$standard_error*2), "</strong>","), ", 
+      #        (sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*2)))/nrow(data()))*100,
+      #        "% of candidates will pass the exam.","<br><br>",
+      #        "When the cut score is decreased by two standard errors (", 
+      #        "<strong>", input$cut_score-(input$standard_error*2), "</strong>","), ", 
+      #        (sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*2)))/nrow(data()))*100,
+      #        "% of candidates will pass the exam.","<br>",
+      #        "</h4>")
+      #     }
+      #     else if(input$SE_num==3){
+      #       paste0("<h4>",
+      #              "When the cut score is increased by three standard errors (", 
+      #              "<strong>", input$cut_score+input$standard_error, "</strong>","), ", 
+      #              (sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*3)))/nrow(data()))*100,
+      #              "% of candidates will pass the exam.","<br><br>",
+      #              "When the cut score is decreased by three standard errors (", 
+      #              "<strong>", input$cut_score-input$standard_error, "</strong>","), ", 
+      #              (sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*3)))/nrow(data()))*100,
+      #              "% of candidates will pass the exam.","<br>",
+      #              "</h4>")
+      #     }
+      #   }
+      # }
+      # else {
+      #   paste("")
+      # }
+    # })
     
     
     data_SE <- reactive({
       req(input$cut_score, input$standard_error, cancelOutput = TRUE)
-      SE_table <- data.frame("Cut Score" = c(input$cut_score+(input$standard_error*3),input$cut_score+(input$standard_error*2), 
-                                             input$cut_score+(input$standard_error*1),input$cut_score,input$cut_score-(input$standard_error*1),
-                                             input$cut_score-(input$standard_error*2),input$cut_score-(input$standard_error*3)),
-                             "Percent Passing" = c(sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*3)))/nrow(data())*100,
+      
+       SE_rounded <- input$standard_error
+      
+      SE_table <- data.frame("Cut Score" = 
+                                  round(c(input$cut_score+(SE_rounded*3),input$cut_score+(SE_rounded*2), 
+                                          input$cut_score+(SE_rounded*1),input$cut_score,input$cut_score-(SE_rounded*1),
+                                          input$cut_score-(SE_rounded*2),input$cut_score-(SE_rounded*3)),0),
+                             "Percent Passing" = 
+                                  round(c(sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*3)))/nrow(data())*100,
                                    sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*2)))/nrow(data())*100,
                                    sum(data()[2]>((input$cut_score-0.01)+(input$standard_error*1)))/nrow(data())*100,
                                    sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*0)))/nrow(data())*100,
                                    sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*1)))/nrow(data())*100,
                                    sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*2)))/nrow(data())*100,
-                                   sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*3)))/nrow(data())*100),
+                                   sum(data()[2]>((input$cut_score-0.01)-(input$standard_error*3)))/nrow(data())*100), 2),
                              check.names = FALSE)
-      
-      row.names(SE_table) <- c("Plus Three SE of R3 Angoff Ratings", "Plus Two SE of R3 Angoff Ratings","Plus One SE of R3 Angoff Ratings",
-                               "Panel-Recommended Raw Cut","Minus One SE of R3 Angoff Ratings", "Minus Two SE of R3 Angoff Ratings", "Minus Three SE of R3 Angoff Ratings")
+
+      # Cut score should not have any decimals -- will have two by default
+      SE_table[1,] <- as.character(SE_table[1,])
+
+      row.names(SE_table) <- c("+3 Standard Error", "+2 Standard Error","+1 Standard Error",
+                               "Panel-Recommended Raw Cut","-1 Standard Error", "-2 Standard Error", "-3 Standard Error")
       return(SE_table)
     })
 
@@ -196,9 +240,10 @@ server <- shinyServer(function(input, output, session) {
       data_SE()},
       rownames = TRUE,
       colnames = TRUE,
-      striped = TRUE,
-      bordered = TRUE, digits = 2
-    )
+      # striped = TRUE,
+      hover = TRUE,
+      bordered = TRUE, 
+    ) 
 })
 
 shinyApp(ui, server)
